7ea21afa099a06e42b362782d6c6fe8f32911e79
Mason M <m8mamiya@gmail.com>
2021-02-04 18:57:18 -0800

    fix index.js error in copy pasting.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fs = require("fs");
 
 var app = express();
-var PORT = process.env.PORT || 8080
+var PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
