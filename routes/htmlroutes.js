f38d8889388dc5e1625a92806c48bf57b4f8ef96
Mason M <m8mamiya@gmail.com>
2021-02-04 18:54:11 -0800

    edited routes folder

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,20 +1,12 @@
-//requires
-const path = require('path');
-
+const path = require("path");
 
 module.exports = function(app) {
-  // HTML GET Requests
-
-  app.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-  });
-
-  app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
-
-  // If no matching route is found default to index
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
-};
+    
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/index.html"));
+    });
+    
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/notes.html"));
+    });
+}
